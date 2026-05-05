6e659a0e6dede890c5f6e0453a1e58c2b332d7b5
/\n!KeT KuMaR <ankitrazz001@gmail.com>
2026-05-05 23:51:19 +0530

    admin login desktop

--- apps/web/app/employe/dashboard/page.tsx
+++ apps/web/app/employe/dashboard/page.tsx
@@ -1,14 +1,29 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Sidebar from "../../component/sidebar";
 
+
 export default function EmployeeDashboard() {
+  const [loading, setloading] = useState(true);
+  const [data, setdata] = useState(null);
+
   useEffect(() => {
     document.body.style.overflow = "hidden";
     return () => {
       document.body.style.overflow = "auto";
     };
   }, []);
+
+
+  if(loading) {
+    return(
+      <p>Loading..</p>
+    )
+  }
+
+  if(!data){
+    return (<p>Data not found </p>)
+  }
 
   return (
     <div style={{ height: "100vh", display: "flex" }}>

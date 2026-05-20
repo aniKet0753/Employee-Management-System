3aba7cf86179765841d562074747a174f2078dfc
/\n!KeT KuMaR <ankitrazz001@gmail.com>
2026-05-20 23:58:20 +0530

    dasktop data

--- apps/web/app/component/Employdashboard.tsx
+++ apps/web/app/component/Employdashboard.tsx
@@ -15,18 +15,21 @@ export default function EmployeeDashboard ({data}: dummyEmployeeDashboardData) {
       icon: <Calendar1Icon  color="white"/>,
       value: data.currentMonthAttendance,
       title: "Days Present",
-      subtitle: "This month"
+      subtitle: "This month",
+      path: "/attendance",
     },
     {
       icon: <FileTextIcon  color="white"/>,
       value: data.pendingLeaves,
       title: "Pending Leaves",
-      subtitle: "Awaiting approval"
+      subtitle: "Awaiting approval",
+      path: "/leaves",
     },{
        icon: <DollarSignIcon  color="white"/>,
-      value: data.latestPayslip ? `$${data.latestPayslip.netSalary?.toLocaleString()}` : "N/A",
+      value: data.latestPayslip ? `${data.latestPayslip.netSalary?.toLocaleString()}` : "N/A",
       title: "Latest Payslip",
-      subtitle: "Most Recent Payment"
+      subtitle: "Most Recent Payment",
+      path:"/payslip",
     }
   ]
   return (
@@ -34,13 +37,13 @@ export default function EmployeeDashboard ({data}: dummyEmployeeDashboardData) {
     <div style={{paddingLeft: "20px",color: "white", fontFamily: "-moz-initial", }}>
       <h2 style={{ color: "white",  fontSize: "30px", marginBottom:"6px"}}> Welcome {emp?.firstName} {emp?.lastName}</h2>
       <p style={{  color: "white", fontSize: "18px", marginTop:"0px"}}>{emp?.position} {emp?.department || " NO DEPARTMENT "}</p>
-      <div> 
+      <div style={{display:"flex", gap:"90px"}}> 
         {card.map((item) => (
-          <div key={item.title} style={{display: "inline-block",gap:"", backgroundColor: "#2c2c2c", borderRadius: "8px", padding: "20px", marginRight: "20px", width: "200px"}}>
-            <div style={{display: "flex", alignItems: "center", marginBottom: "10px"}}>{item.icon}</div>
-            <h3 style={{ color: "white", fontSize: "24px", margin: "0" }}>{item.value}</h3>
-            <p style={{ color: "gray", fontSize: "14px", margin: "0" }}>{item.title}</p>
-            <p style={{ color: "lightgray", fontSize: "12px", margin: "0" }}>{item.subtitle}</p>
+          <div key={item.title} onClick={() => (item.path)} style={{cursor:"pointer",marginLeft:"30px",display: "inline-block",gap:"20px", backgroundColor: "rgb(44, 44, 44)", borderRadius: "8px", padding: "20px", marginRight: "20px", width: "200px"}}>
+            <div style={{display: "flex", alignItems: "center", marginBottom: "7px" }}>{item.icon}
+            <h3 style={{ color: "white", fontSize: "24px", margin: "0", marginLeft: "5px" }}>{item.value}</h3></div>
+            <p style={{ color: "gray", fontSize: "14px", margin: "0", fontFamily:"inherit" }}>{item.title}</p>
+            <p style={{ color: "lightgray", fontSize: "12px", margin: "0", fontFamily:"-moz-initial" , fontWeight:"bold" }}>{item.subtitle}</p>
           </div>
         ))}
       </div>

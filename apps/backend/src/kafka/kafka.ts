2f83195caaf4432a3fa9a0be5f7cf0796ae07117
/\n!KeT KuMaR <ankitrazz001@gmail.com>
2026-08-23 23:39:01 +0530

    kafka ssl path

--- apps/backend/src/kafka/kafka.ts
+++ apps/backend/src/kafka/kafka.ts
@@ -1,18 +1,13 @@
 import "dotenv/config";
 import fs from "node:fs";
 import {Kafka} from "kafkajs"
-import { fileURLToPath } from "node:url";
 
-
-const caPath = fileURLToPath(
-  new URL("../../src/certs/ca.pem", import.meta.url)
-);
 
 export const KAFKA = new Kafka({
   clientId : "employee-management",
   brokers: [process.env.KAFKA_BROKER!],
   ssl:{
-    ca: [fs.readFileSync(caPath, "utf-8")],
+    ca:[process.env.KAFKA_CA_CERT!]
   },
   sasl:{
     mechanism: "scram-sha-256",

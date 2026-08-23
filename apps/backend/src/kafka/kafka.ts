eb98264e98ecab76859218833ddad70e88c106df
/\n!KeT KuMaR <ankitrazz001@gmail.com>
2026-08-23 22:08:12 +0530

    fixed kafka path on dployee

--- apps/backend/src/kafka/kafka.ts
+++ apps/backend/src/kafka/kafka.ts
@@ -1,12 +1,18 @@
 import "dotenv/config";
 import fs from "node:fs";
 import {Kafka} from "kafkajs"
+import { fileURLToPath } from "node:url";
+
+
+const caPath = fileURLToPath(
+  new URL("../../src/certs/ca.pem", import.meta.url)
+);
 
 export const KAFKA = new Kafka({
   clientId : "employee-management",
   brokers: [process.env.KAFKA_BROKER!],
   ssl:{
-    ca: [fs.readFileSync("./src/certs/ca.pem","utf-8")]
+    ca: [fs.readFileSync(caPath, "utf-8")],
   },
   sasl:{
     mechanism: "scram-sha-256",
